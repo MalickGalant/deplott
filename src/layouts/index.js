bc1f1ac6cbd240885e8d7c04d42973d956fac72e
MalickGalant <89748688+MalickGalant@users.noreply.github.com>
2023-02-22 19:56:17 +0200

    Update index.js

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -23,14 +23,14 @@ export default class Template extends React.Component {
         <div
           style={{
             background: `rebeccapurple`,
-            marginBottom: `1.45rem`,
+            marginBottom: `1.25rem`,
           }}
         >
           <div
             style={{
               margin: `0 auto`,
               maxWidth: 960,
-              padding: `1.45rem 1.0875rem`,
+              padding: `1.25rem 1.0875rem`,
             }}
           >
             <h1 style={{ margin: 0 }}>
@@ -41,7 +41,7 @@ export default class Template extends React.Component {
                   textDecoration: "none",
                 }}
               >
-                Gatsby
+                Deplott
               </Link>
             </h1>
           </div>
@@ -50,7 +50,7 @@ export default class Template extends React.Component {
           style={{
             margin: `0 auto`,
             maxWidth: 960,
-            padding: `0px 1.0875rem 1.45rem`,
+            padding: `0px 1.0875rem 1.25rem`,
             paddingTop: 0,
           }}
         >
